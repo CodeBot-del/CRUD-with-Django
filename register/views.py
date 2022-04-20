c38ba363b12ec2cf30315ca8ffe973ce58bbba78
gracie2 <stevenmanangu360@gmail.com>
2022-04-20 13:51:04 +0300

    working the templates

--- register/views.py
+++ register/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def employee_list(request):
+    return render(request, "register/employee_list.html")
+
+def employee_form(request):  #for create and update employee
+    return render(request, "register/employee_form.html")
+
+def employee_delete(request):
+    return
