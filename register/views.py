a3cb8feb691d26a15ca5e565e28d33c1a7e2fbaa
gracie2 <stevenmanangu360@gmail.com>
2022-04-20 18:21:39 +0300

    successfully updated details

--- register/views.py
+++ register/views.py
@@ -16,9 +16,14 @@ def employee_form(request, id=0):  #for create and update employee
             form = EmployeeForm()
         else:
             employee = Employee.objects.get(pk=id) #get the corresponding id to update
+            form = EmployeeForm(instance=employee)
         return render(request, "register/employee_form.html", {'form': form})
     else:
-        form = EmployeeForm(request.POST)
+        if id == 0:
+            form = EmployeeForm(request.POST) #for create
+        else:
+            employee = Employee.objects.get(pk=id) #for update
+            form = EmployeeForm(request.POST,instance = employee)
         if form.is_valid():
             form.save()
         return redirect('/employee/list')
