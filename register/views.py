4fcf4b4b9fa10bdf343f6309e415d4a6407e3621
gracie2 <stevenmanangu360@gmail.com>
2022-04-20 14:23:32 +0300

    displayed the form from the model

--- register/views.py
+++ register/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
+from .forms import EmployeeForm
 
 # Create your views here.
 def employee_list(request):
+    
     return render(request, "register/employee_list.html")
 
+
+
 def employee_form(request):  #for create and update employee
-    return render(request, "register/employee_form.html")
+    
+    form = EmployeeForm()
+    
+    return render(request, "register/employee_form.html", {'form': form})
+
+
 
 def employee_delete(request):
+    
     return
