c5f737fb17321f98a8385699c38aea561372baf6
gracie2 <stevenmanangu360@gmail.com>
2022-04-20 15:53:19 +0300

    added crispy forms

--- register/forms.py
+++ register/forms.py
@@ -4,4 +4,6 @@ from .models import Employee
 class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
-        fields = '__all__'
+        # fields = '__all__' #get all the fields from model
+        fields = ('fullname', 'mobile', 'emp_code', 'position')
+        
