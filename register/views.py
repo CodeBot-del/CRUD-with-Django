7a5ea0ad02b2ee7fc5d6360e2d1e40e51de6b510
gracie2 <stevenmanangu360@gmail.com>
2022-04-20 18:02:29 +0300

    trying to update information

--- register/views.py
+++ register/views.py
@@ -1,18 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import EmployeeForm
+from .models import Employee
 
 # Create your views here.
 def employee_list(request):
+    context = {'employee_list': Employee.objects.all()}
     
-    return render(request, "register/employee_list.html")
+    return render(request, "register/employee_list.html", context)
 
 
 
-def employee_form(request):  #for create and update employee
-    
-    form = EmployeeForm()
-    
-    return render(request, "register/employee_form.html", {'form': form})
+def employee_form(request, id=0):  #for create and update employee
+    if request.method == "GET":
+        if id == 0:
+            form = EmployeeForm()
+        else:
+            employee = Employee.objects.get(pk=id)
+        return render(request, "register/employee_form.html", {'form': form})
+    else:
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/employee/list')
 
 
 
